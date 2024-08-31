c8687212dbd7683f577ceea1074486dba71477ce
JoshKoledoye <joshkoledoyejr@gmail.com>
2024-09-01 00:46:24 +0100

    website
    ..

---
+++ Scripts/projects.js
@@ -0,0 +1,1 @@
+alert("View our projects here!")
---
+++ Scripts/script.js
@@ -0,0 +1,3 @@
+user_input = prompt("Hi there! what's your name?");
+alert("Welcome to the official Krimson studios website, "+ user_input);
+alert("We hope you enjoy our website");
